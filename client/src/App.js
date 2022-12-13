259494790d80ba2b2997202824f2a9cd3d037788
MarkAnthony9014 <markanthony9014@gmail.com>
2022-12-12 23:49:09 -0600

    boilerplate code

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
+function App() {
+  return (
+   <ApolloProvider client={client}>
+    <Router>
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    </Router>
+   </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,3 @@
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+import Auth from '../utils/auth';
+import FriendList from '../components/FriendList';
