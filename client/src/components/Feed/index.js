a510b2e7358811134ad8882529c06338251901fe
MarkAnthony9014 <markanthony9014@gmail.com>
2022-12-14 20:33:38 -0600

    pushing

---
+++ client/src/components/Feed/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Feed = ({ posts, title }) => {
+    if (!posts.length) {
+      return <h3>No Thoughts Yet</h3>;
+    }
+
+return (
+    <div>
+        <h3>{title}</h3>
+    </div>
+);
+
+
+};
+
+
+export default Feed;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import Feed from '../components/Feed';
+import FriendList from '../components/FriendList';
+
 import { useQuery } from '@apollo/client';
 import { QUERY_POSTS, QUERY_ME_BASIC } from '../utils/queries';
 import Auth from '../utils/auth';
-import FriendList from '../components/FriendList';
+
+const Home = () => {
+    const { loading, data } = useQuery(QUERY_POSTS);
+    const { data: userData } = useQuery(QUERY_ME_BASIC);
+    const posts = data?.posts || [];
+
+    const loggedIn = Auth.loggedIn();
+
+  return (
+      <main>
+        <div className="">
+            {loggedIn && (
+                <div className="col-12 mb-3">
+                </div>   
+            )}
+        </div>
+      </main>
+  );  
+}
