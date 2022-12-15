7d57d77b9c73dc82fcb969ac35f8e749dd59b10c
MarkAnthony9014 <markanthony9014@gmail.com>
2022-12-14 20:42:50 -0600

    pushing again

--- client/src/pages/Home.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
-import Auth from '../utils/auth';
-import FriendList from '../components/FriendList';
