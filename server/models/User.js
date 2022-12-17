491f2419f0138a8ddc083b54731b12e326eb8e15
Robert Martin <rlmartin1996@gmail.com>
2022-12-16 21:40:02 -0600

    post

--- server/models/User.js
+++ server/models/User.js
@@ -20,10 +20,10 @@ const userSchema = new Schema(
       required: true,
       minlength: 5
     },
-    posts: [
+    thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Post'
+        ref: 'Thoughts'
       }
     ],
     friends: [
