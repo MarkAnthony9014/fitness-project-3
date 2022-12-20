23a916c43d676becfe4dbc752c7318d0f7b5cf1c
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-12-20 04:54:52 -0600

    added more posts for tests, category doesnt show

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 import PostForm from '../components/PostForm';
 import PostList from '../components/PostList';
 import FriendList from '../components/FriendList';
 
+
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import { ADD_FRIEND } from '../utils/mutations';
-import Auth from '../utils/auth';
+
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
